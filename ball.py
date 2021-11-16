5f8c664b7db30f8e056ca762dbf7284b7b7d171f
AAKK888 <89204848+AAKK888@users.noreply.github.com>
2021-11-16 16:36:12 -0600

    Create ball.py

---
+++ ball.py
@@ -0,0 +1,28 @@
+import pygame 
+import random
+import vari
+pygame.init()
+
+class Ball(pygame.sprite.Sprite):
+  def __init__(self, x_speed, y_speed):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.Surface((20,20))
+    self.image.fill(vari.RED)
+    self.image.set_colorkey(vari.BLACK)
+    self.rect = self.image.get_rect()
+    self.rect.center = (random.randint(5, 200), random.randint(5, 200))
+    self.x_speed = x_speed
+    self.y_speed = y_speed
+    self.number = 0
+
+  def update(self):
+    self.rect.x += self.x_speed
+    self.rect.y += self.y_speed
+    if self.rect.bottom < vari.HEIGHT:
+      self.y_speed = -self.y_speed
+    if self.rect.top > 0:
+      self.y_speed = -self.y_speed
+    if self.rect.right > vari.WIDTH:
+      self.x_speed = -self.x_speed
+    if self.rect.left < 0:
+      self.x_speed = -self.x_speed
