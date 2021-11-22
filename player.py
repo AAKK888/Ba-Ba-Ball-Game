7cb7287d660bf6b06ec7ae9c8366d9bf711ce28b
AAKK888 <89204848+AAKK888@users.noreply.github.com>
2021-11-22 08:03:30 -0600

    Update player.py

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ pygame.init()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((25, 25))
+        self.image = pygame.Surface((20, 20))
         self.image.fill(vari.BLUE)
         self.image.set_colorkey(vari.BLACK)
         self.rect = self.image.get_rect()
@@ -21,13 +21,13 @@ class Player(pygame.sprite.Sprite):
         self.speedy = 0
         keypress = pygame.key.get_pressed()
         if keypress[pygame.K_a] or keypress[pygame.K_LEFT]:
-            self.speedx = -4
+            self.speedx = -5
         if keypress[pygame.K_d] or keypress[pygame.K_RIGHT]:
-            self.speedx = 4
+            self.speedx = 5
         if keypress[pygame.K_w] or keypress[pygame.K_UP]:
-            self.speedy = -4
+            self.speedy = -5
         if keypress[pygame.K_s] or keypress[pygame.K_DOWN]:
-            self.speedy = 4
+            self.speedy = 5
 
         if self.speedx != 0 and self.speedy != 0:
             self.speedx /= 1.414
