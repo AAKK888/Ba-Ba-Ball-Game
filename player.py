b7cdc1cd0422d20bfb1c24500de323ecb9dcbea2
AAKK888 <89204848+AAKK888@users.noreply.github.com>
2022-08-14 09:55:37 -0500

    Update player.py

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-import vari
+import t
 
 pygame.init()
 
@@ -9,10 +9,10 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((20, 20))
-        self.image.fill(vari.BLUE)
-        self.image.set_colorkey(vari.BLACK)
+        self.image.fill(t.BLUE)
+        self.image.set_colorkey(t.BLACK)
         self.rect = self.image.get_rect()
-        self.rect.center = (vari.WIDTH / 2, vari.HEIGHT / 2)
+        self.rect.center = (t.WIDTH / 2, t.HEIGHT / 2)
         self.speedx = 0
         self.speedy = 0
 
@@ -21,24 +21,24 @@ class Player(pygame.sprite.Sprite):
         self.speedy = 0
         keypress = pygame.key.get_pressed()
         if keypress[pygame.K_a] or keypress[pygame.K_LEFT]:
-            self.speedx = -5
+            self.speedx = -6
         if keypress[pygame.K_d] or keypress[pygame.K_RIGHT]:
-            self.speedx = 5
+            self.speedx = 6
         if keypress[pygame.K_w] or keypress[pygame.K_UP]:
-            self.speedy = -5
+            self.speedy = -6
         if keypress[pygame.K_s] or keypress[pygame.K_DOWN]:
-            self.speedy = 5
+            self.speedy = 6
 
         if self.speedx != 0 and self.speedy != 0:
             self.speedx /= 1.414
             self.speedy /= 1.414
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        if self.rect.right > vari.WIDTH:
-            self.rect.left = 0
+        if self.rect.right > t.WIDTH:
+            self.rect.left = t.WIDTH - 75
         if self.rect.left < 0:
-            self.rect.right = vari.WIDTH
-        if self.rect.bottom > vari.HEIGHT:
-            self.rect.top = 0
+            self.rect.right = 75
+        if self.rect.bottom > t.HEIGHT:
+            self.rect.top = t.HEIGHT - 75
         if self.rect.top < 0:
-            self.rect.bottom = vari.HEIGHT
+            self.rect.bottom = 75
